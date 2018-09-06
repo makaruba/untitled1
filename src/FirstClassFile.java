74c0c125b7455fd8396c9adb8af25d3cb452abf6
Vitalii <makaruba@mail.ru>
2018-09-06 12:54:42 +0700

    third commit

--- src/FirstClassFile.java
+++ src/FirstClassFile.java
@@ -4,5 +4,6 @@ public class FirstClassFile {
     public static void main(String[] args) {
         out.println("Privet AlenaQA!");
         System.out.println("ciao Vitalii");
+        System.out.println("Hallo");
     }
 }
